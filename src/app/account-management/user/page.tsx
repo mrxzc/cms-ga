8377c40997f43e426011ee1e8c64fa2949b4995d
Azriel Setyo <86305687+Stay2607@users.noreply.github.com>
2024-08-23 16:19:57 +0700

    add: menambahkan page user dan role management, juga membenarkan menu cms

---
+++ src/app/account-management/user/page.tsx
@@ -0,0 +1,13 @@
+import { UserPage } from '@components/organisms/Account'
+
+export const metadata = {
+  title: 'Management User',
+}
+
+export default function ManagingUser() {
+  return (
+    <>
+      <UserPage />
+    </>
+  )
+}
---
+++ src/components/organisms/Account/User/index.tsx
@@ -0,0 +1,10 @@
+import { User } from '@components/molecules/Account'
+import React from 'react'
+
+export function UserPage() {
+  return (
+    <div>
+      <User></User>
+    </div>
+  )
+}
